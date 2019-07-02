11fa0c30c0d69d054e94aab85a92280aebd912cd
kelin <1210745444@qq.com>
2019-07-02 12:08:03 +0800

    promise 完整办，有问题

--- noteBox/promise/promise.js
+++ noteBox/promise/promise.js
@@ -1,6 +1,4 @@
-;
-var a=0;
-console.log(a)
+
 class MyPromise{
 	constructor(excutor) {
 		let self = this;
@@ -11,26 +9,34 @@ class MyPromise{
 		this.rejectedCb = [];
 		
 		let resolve = function(data){
-			if(data.then!==null&&(typeof data.then == "object" || typeof data.then == "function")){
-				return data.then()
-			}
+			// if (value.then !== null && (typeof value.then === 'object' || typeof value.then === 'function')) 
+			// return value.then(resolve, reject)
 			
-			self.status = "fulfilled";
-			self.value = data;
+			if(data.then!==null&&(typeof data.then == "object" || typeof data.then == "function"))
+			return data.then(resolve, reject)
+				
 			setTimeout(()=>{
-				self.fulfilledCB.forEach(fn=>{
-					fn()
-				})
+				if(self.status == "pending"){
+					self.status = "fulfilled";
+					self.value = data;
+					self.fulfilledCB.forEach(fn=>{
+						fn()
+					})
+				}
+				
 			})
 			
 		}
 		let reject = function(err){
-			self.status = "rejected";
-			self.reason = err;
 			setTimeout(()=>{
-				self.rejectedCb.forEach(fn=>{
-					fn()
-				})
+				if(self.status=="pending"){
+					self.status = "rejected";
+					self.reason = err;
+					self.rejectedCb.forEach(fn=>{
+						fn()
+					})
+				}
+				
 			})
 		}
 		try{
@@ -41,20 +47,39 @@ class MyPromise{
 		}
 		
 	}
-	then(done,fail){
+	then(done =value => value,fail = reson => {
+					throw reason
+				}){
 		let promise2;
+		let self = this;
+		let isCalled = false;
 		let resolvePromise = function(promise2,x,resolve,reject){
-			if(promise2 == x) throw new Error("循环调用！")
-			if(typeof x == "function" || typeof x == "object" && x!==null){
-				let then = x.then;
+			if(promise2 == x) throw new Error("循环调用！");
+			// if(x instanceof MyPromise){
+			// 	if(x.status == "pending"){
+			// 		x.then(y=>{
+			// 			resolvePromise(promise2,y,resolve,reject)
+			// 		})
+			// 	}else{
+			// 		x.then(resolve,reject)
+			// 	}
+			// }
+			if(typeof x == "function" || typeof x == "object" && x!==null){	
 				if(typeof then == "function"){
+					let then = x.then;
 					try{
 						then.call(x,res=>{
-							resolveMyPromise(promise2,res,resolve,reject)
+							if(isCalled) return
+							isCalled = true
+							resolvePromise(promise2,res,resolve,reject)
 						},err=>{
+							if(isCalled) return
+							isCalled = true
 							reject(err)
 						})
 					}catch(e){
+						if(isCalled) return
+						isCalled = true
 						reject(err)
 						//TODO handle the exception
 					}
@@ -66,21 +91,84 @@ class MyPromise{
 			}
 		}
 		if(self.status == "fulfilled"){
-			try{
-				let x = done(self.value);
-				promise2 = new MyPromise((resolve,reject)=>{
-					resolvePromise(promise2,x,resolve,reject)
+		return	(promise2 = new MyPromise((resolve,reject)=>{
+				setTimeout(()=>{
+						try{
+							let x = done(self.value);
+							resolvePromise(promise2,x,resolve,reject)
+					}catch(e){
+						//TODO handle the exception
+						throw new Error("成功函数有误!")
+					}
 				})
-			}catch(e){
-				//TODO handle the exception
-				throw new Error("成功函数有误!")
-			}
+			}))
 		}
+		if(self.status == "rejected"){
+		 return	(promise2 = new MyPromise((resolve,reject)=>{				
+				setTimeout(()=>{
+					try{
+							let x = fail(self.value);
+							resolvePromise(promise2,x,resolve,reject)	
+					}catch(e){
+						//TODO handle the exception
+						throw new Error("成功函数有误!")
+					}
+				})			
+			}))
+			
+		}
+		if(self.status == "pending"){
+		
+			return (promise2 = new MyPromise((resolve,reject)=>{
+				self.fulfilledCB.push(()=>{	
+						try{
+							let x = done(self.value);
+							resolvePromise(promise2,x,resolve,reject)
+						}catch(e){
+							//TODO handle the exception
+							throw new Error("成功函数有误!")
+						}
+					
+				})
+				self.rejectedCb.push(()=>{					
+						try{
+							let x = fail(self.reason);
+							resolvePromise(promise2,x,resolve,reject)
+						}catch(e){
+							//TODO handle the exception
+							throw new Error("成功函数有误!")
+						}					
+				})
+			}))			
+		}
+	
 	}
 }
 var demo = new MyPromise((resolve,reject)=>{
-	resolve(100)
+	// debugger
+	if(Math.random()>0.2){
+		setTimeout(()=>{
+			resolve(100)
+		},1000)	
+	}else{
+		reject(200)
+	}
+})
+// console.dir(demo)
+
+demo.then(res=>{
+	console.log(res)
+		return new Promise((resolve,reject)=>{	
+			setTimeout(()=>{
+					resolve(res+"第一次成功！")
+				},1000)})		
+},err=>{
+	console.log("error" + err)
+}).then(res=>{
+	console.log(res)
+},err=>{
+	console.log(err)
 })
 
-console.dir(demo)
-// module.exports.MyPromise = MyPromise;
+
+
